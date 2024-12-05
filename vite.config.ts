ec533cf95d8f4f99628a448f26cc8088cb71fad7
terrence-ou <outerrencedl@gmail.com>
2024-12-05 16:12:10 -0500

    deployment updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Bento-News-Website/",
   plugins: [react()],
   resolve: {
     alias: {
