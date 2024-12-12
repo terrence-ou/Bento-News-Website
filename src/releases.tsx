f5c9602b09c7a17920e2f8957df533d6575c68de
terrence-ou <outerrencedl@gmail.com>
2024-12-12 16:10:31 -0500

    home page updated

---
+++ src/releases.tsx
@@ -0,0 +1,21 @@
+export const releases = [
+  {
+    version: "0.2.1",
+    date: "12/12/2024",
+    changes: {},
+    downloads: {
+      macOS: {
+        versions: [
+          [
+            "Intel chip",
+            "https://github.com/terrence-ou/Bento-News/releases/download/v0.2.1/BentoNews-0.2.1-x64.dmg",
+          ],
+          [
+            "Apple silicon",
+            "https://github.com/terrence-ou/Bento-News/releases/download/v0.2.1/BentoNews-0.2.1-arm64.dmg",
+          ],
+        ],
+      },
+    },
+  },
+];
