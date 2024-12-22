c543615c89624ab0e113b7b06ee1a6d47f21aa38
terrence-ou <outerrencedl@gmail.com>
2024-12-22 16:39:44 -0500

    0.3.1 released

--- src/releases.tsx
+++ src/releases.tsx
@@ -1,5 +1,24 @@
 export const releases = [
   {
+    version: "0.3.1",
+    date: "12/22/2024",
+    changes: {},
+    downloads: {
+      macOS: {
+        versions: [
+          [
+            "Intel chip",
+            "https://github.com/terrence-ou/Bento-News/releases/download/v0.3.1/BentoNews-0.3.1-x64.dmg",
+          ],
+          [
+            "Apple silicon",
+            "https://github.com/terrence-ou/Bento-News/releases/download/v0.3.1/BentoNews-0.3.1-arm64.dmg",
+          ],
+        ],
+      },
+    },
+  },
+  {
     version: "0.3.0",
     date: "12/17/2024",
     changes: {},
